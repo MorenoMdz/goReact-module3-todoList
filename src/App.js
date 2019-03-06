9d8543fea7263fb07a07875779bb396823f2d5c1
Moreno Antunes <morenomdz@gmail.com>
2019-03-06 10:58:58 -0300

    todos list + reactotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import './config/reactotron';
+import store from './store';
+
+import TodoList from './TodoList';
+
+console.tron.log('HW');
+console.tron.warn('HW');
 
 const App = () => (
-  <div className="App">
+  <Provider store={store} className="App">
     <h1>Hello World!</h1>
-  </div>
+    <TodoList />
+  </Provider>
 );
-
-// comment
 
 export default App;
